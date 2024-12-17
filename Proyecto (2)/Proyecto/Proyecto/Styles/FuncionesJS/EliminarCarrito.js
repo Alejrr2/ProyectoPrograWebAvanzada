c002e6e2730bc379ea43e72411bc964686b57a0b
Raquel Ariana Arias Sancho <raquelarias3102@gmail.com>
2024-12-16 21:48:41 -0600

    EliminarCarrito

---
+++ Proyecto (2)/Proyecto/Proyecto/Styles/FuncionesJS/EliminarCarrito.js
@@ -0,0 +1,7 @@
+﻿$(document).on("click", ".abrirModal", function () {
+
+    $("#ConsecutivoProducto").val($(this).attr("data-id"));
+    $("#Nombre").text($(this).attr("data-name"));
+
+    alert(consecutivo + " " + nombre);
+})
